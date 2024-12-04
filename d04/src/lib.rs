4f22b77d8462682144f01f104745c3aa82ad7bb4
boris <bg@voltra-solutions.com>
2024-12-04 14:33:36 +0100

    day4, part one is not happiness inducing

---
+++ d04/src/lib.rs
@@ -0,0 +1,167 @@
+use regex::Regex;
+use std::error::Error;
+use std::fs;
+
+fn rows(input: &str) -> Vec<String> {
+    input
+        .lines()
+        .map(|l| l.to_string())
+        .collect::<Vec<String>>()
+}
+
+fn cols(input: &str) -> Vec<String> {
+    let mut res = Vec::new();
+    input.lines().for_each(|_| res.push(String::new()));
+    for line in input.lines() {
+        for (i, c) in line.chars().enumerate() {
+            res[i].push(c);
+        }
+    }
+    res
+}
+
+fn diag(input: &str) -> Vec<String> {
+    let matrix: Vec<Vec<char>> = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect();
+    let starting_indices = (0..matrix.len())
+        .map(|x| (x, 0))
+        .chain((1..matrix[0].len()).map(|x| (0, x)));
+
+    let mut res = Vec::new();
+    for (mut i, mut j) in starting_indices {
+        let mut line = String::new();
+        loop {
+            if let Some(l) = matrix.get(i) {
+                if let Some(c) = l.get(j) {
+                    line.push(c.clone());
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+            i += 1;
+            j += 1;
+        }
+        res.push(line);
+    }
+
+    let starting_indices = (0..matrix.len())
+        .map(|x| (x, matrix[0].len() - 1))
+        .chain((0..matrix[0].len() - 1).map(|x| (0, x)));
+
+    for (mut i, mut j) in starting_indices {
+        let mut line = String::new();
+        loop {
+            if let Some(l) = matrix.get(i) {
+                if let Some(c) = l.get(j) {
+                    line.push(c.clone());
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+            if j == 0 {
+                break;
+            }
+            i += 1;
+            j -= 1;
+        }
+        res.push(line);
+    }
+    res
+}
+
+fn part1(input: String) -> i32 {
+    let re = Regex::new(r"XMAS").unwrap();
+
+    rows(&input)
+        .iter()
+        .chain(&cols(&input))
+        .chain(&diag(&input))
+        .map(|s| {
+            (re.find_iter(s).count() + re.find_iter(&(s.chars().rev().collect::<String>())).count())
+                as i32
+        })
+        .sum()
+}
+
+fn part2(input: String) -> i32 {
+    let matrix: Vec<Vec<char>> = input
+        .lines()
+        .map(|l| l.chars().collect::<Vec<char>>())
+        .collect();
+
+    let mut res = 0;
+    for i in 1..matrix.len() - 1 {
+        for j in 1..matrix[0].len() - 1 {
+            if matrix[i][j] != 'A' {
+                continue;
+            }
+            let ul = matrix[i - 1][j - 1];
+            let lr = matrix[i + 1][j + 1];
+            let ur = matrix[i - 1][j + 1];
+            let ll = matrix[i + 1][j - 1];
+            if ((ul == 'M' && lr == 'S') || (ul == 'S' && lr == 'M'))
+                && ((ur == 'M' && ll == 'S') || (ur == 'S' && ll == 'M'))
+            {
+                res += 1;
+            }
+        }
+    }
+    res
+}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    let input1 = fs::read_to_string("input1")?;
+    println!("part 1: {}", part1(input1));
+    let input2 = fs::read_to_string("input1")?;
+    println!("part 2: {}", part2(input2));
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn p1_1() {
+        let input = "\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"
+            .to_string();
+        //println!("rows(&input): {:?}", rows(&input));
+        //println!("cols(&input): {:?}", cols(&input));
+        //println!("diag(&input): {:?}", diag(&input));
+        assert_eq!(18, part1(input));
+    }
+
+    #[test]
+    fn p2_1() {
+        let input = "\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX"
+            .to_string();
+        assert_eq!(9, part2(input));
+    }
+}

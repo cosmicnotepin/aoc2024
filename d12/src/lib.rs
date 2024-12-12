5cb448564f3f922961280a2a2030d38656377762
boris <bg@voltra-solutions.com>
2024-12-12 12:45:36 +0100

    d12p2 and cleanup1

--- d12/src/lib.rs
+++ d12/src/lib.rs
@@ -4,41 +4,64 @@ use std::error::Error;
 use std::fs;
 use std::time::Instant;
 
-fn ff(row: usize, col: usize, new_id: usize, map: &mut Vec<Vec<(char, usize)>>) -> (usize, usize) {
-    let fences: HashSet<((usize, usize), (isize, isize))> = HashSet::new();
+fn ff(
+    row: usize,
+    col: usize,
+    new_id: usize,
+    map: &mut Vec<Vec<(char, usize)>>,
+    p1: bool,
+) -> (usize, usize) {
+    let mut fences: HashSet<((isize, isize), (isize, isize))> = HashSet::new();
     let mut todo = VecDeque::new();
-    todo.push_front((row, col));
+    todo.push_front((row as isize, col as isize));
     map[row][col].1 = new_id;
     let (plant, _) = map[row][col];
     let row_count = map.len() as isize;
     let col_count = map[0].len() as isize;
-    let mut fence_count = 0;
     let mut fields = 1;
     while todo.len() > 0 {
         let (row, col) = todo.pop_front().unwrap();
-        for (nr, nc) in [(-1, 0), (0, 1), (1, 0), (0, -1)]
-            .iter()
-            .map(|(ro, co)| (row as isize + ro, col as isize + co))
-        {
+        for (ro, co) in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
+            let nr = row + ro;
+            let nc = col + co;
             if !(0..row_count).contains(&nr) || !(0..col_count).contains(&nc) {
-                fence_count += 1;
+                fences.insert(((row, col), (ro, co)));
                 continue;
             }
-            let nr = nr as usize;
-            let nc = nc as usize;
-            let (n_plant, n_id) = map[nr][nc];
+            let (n_plant, ref mut n_id) = map[nr as usize][nc as usize];
             if n_plant != plant {
-                fence_count += 1;
+                fences.insert(((row, col), (ro, co)));
                 continue;
             }
-            if n_id != new_id {
-                map[nr][nc].1 = new_id;
+            if *n_id != new_id {
+                *n_id = new_id;
                 fields += 1;
                 todo.push_back((nr, nc));
             }
         }
     }
-    return (fields, fence_count);
+    if p1 {
+        return (fields, fences.len());
+    }
+
+    let mut side_count = 0;
+    while fences.len() > 0 {
+        let mut todo: VecDeque<((isize, isize), (isize, isize))> = VecDeque::new();
+        let fence = fences.iter().next().unwrap().clone();
+        side_count += 1;
+        fences.remove(&fence);
+        todo.push_front(fence);
+        while todo.len() > 0 {
+            let ((r, c), (ro, co)) = todo.pop_front().unwrap();
+            for n_f in [((r - co, c + ro), (ro, co)), ((r + co, c - ro), (ro, co))] {
+                if fences.contains(&n_f) {
+                    todo.push_back(n_f);
+                    fences.remove(&n_f);
+                }
+            }
+        }
+    }
+    return (fields, side_count);
 }
 
 fn part1(input: String) -> usize {
@@ -52,7 +75,7 @@ fn part1(input: String) -> usize {
     for r in 0..map.len() {
         for c in 0..map[0].len() {
             if map[r][c].1 == 0 {
-                let (fields, fences) = ff(r, c, init_id, &mut map);
+                let (fields, fences) = ff(r, c, init_id, &mut map, true);
                 init_id += 1;
                 res += fields * fences;
             }
@@ -61,8 +84,24 @@ fn part1(input: String) -> usize {
     return res;
 }
 
-fn part2(input: String) -> i32 {
-    return input.len().try_into().unwrap();
+fn part2(input: String) -> usize {
+    let mut init_id = 1;
+    let mut map: Vec<Vec<(char, usize)>> = input
+        .lines()
+        .map(|l| l.chars().map(|c| (c, 0)).collect())
+        .collect();
+    println!("before ff");
+    let mut res = 0;
+    for r in 0..map.len() {
+        for c in 0..map[0].len() {
+            if map[r][c].1 == 0 {
+                let (fields, sides) = ff(r, c, init_id, &mut map, false);
+                init_id += 1;
+                res += fields * sides;
+            }
+        }
+    }
+    return res;
 }
 
 pub fn run() -> Result<(), Box<dyn Error>> {
@@ -124,7 +163,54 @@ MMMISSJEEE"
 
     #[test]
     fn p2_1() {
-        let input = "".to_string();
-        assert_eq!(0, part2(input));
+        let input = "\
+AAAA
+BBCD
+BBCC
+EEEC"
+            .to_string();
+        assert_eq!(80, part2(input));
+    }
+
+    #[test]
+    fn p2_2() {
+        let input = "\
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO"
+            .to_string();
+        assert_eq!(436, part2(input));
+    }
+
+    #[test]
+    fn p2_3() {
+        let input = "\
+AAAAAA
+AAABBA
+AAABBA
+ABBAAA
+ABBAAA
+AAAAAA"
+            .to_string();
+        assert_eq!(368, part2(input));
+    }
+
+    #[test]
+    fn p2_4() {
+        let input = "\
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE"
+            .to_string();
+        assert_eq!(1206, part2(input));
     }
 }
